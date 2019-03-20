b4aeb6beb359f03d218c2e758cae741bcae044be
Himanshu Ranjan <h4himanshu.ranjan@gmail.com>
2019-03-20 22:29:14 +0530

    Adding final files

--- client2.c
+++ client2.c
@@ -1,6 +1,6 @@
 #include "requirement.h"
 
-void socketcreate(int *socket_fd,int domain,int type,int proto)
+void socketcreate(int *socket_fd,int domain,int type,int proto) // function to create a new socket
 {
     if ((*socket_fd = socket (domain, type, proto)) <0)
         {
@@ -9,7 +9,7 @@ void socketcreate(int *socket_fd,int domain,int type,int proto)
         }
 }
 
-void socketconnect(int *socket_fd,struct sockaddr_in *server_addr)
+void socketconnect(int *socket_fd,struct sockaddr_in *server_addr) // socket is connected to the server address 
 {
     if (connect(*socket_fd, (struct sockaddr *) & *server_addr, sizeof(*server_addr))<0)
         {
@@ -18,7 +18,7 @@ void socketconnect(int *socket_fd,struct sockaddr_in *server_addr)
         }
 }
 
-void messagerecieve(int *socket_fd,struct message *msg)
+void messagerecieve(int *socket_fd,struct message *msg) // function to recieve message 
 {
     if (recv(*socket_fd, &(*msg), sizeof(*msg),0) == 0)
     {
@@ -30,7 +30,7 @@ void messagerecieve(int *socket_fd,struct message *msg)
 
 int main(int argc, char **argv)
 {
-	if (argc !=3)
+	if (argc !=3) // if argument is not correct then terminate
     {
         printf("Server address or port no. missing\nensure: <filename> <server address> <server port>\n");
         exit(1);
@@ -41,27 +41,28 @@ int main(int argc, char **argv)
 	socklen_t servlen;
    
 
-    socketcreate(&socket_fd,AF_INET, SOCK_STREAM,0);
+    socketcreate(&socket_fd,AF_INET, SOCK_STREAM,0); // socket is created 
 
 
-    memset(&server_addr, 0, sizeof(server_addr));
+    memset(&server_addr, 0, sizeof(server_addr));// server address initalized to 0
     server_addr.sin_family = AF_INET;
     server_addr.sin_addr.s_addr= inet_addr(argv[1]);
     server_addr.sin_port =  htons(server_port_no);
 
-    socketconnect(&socket_fd,&server_addr);
+    socketconnect(&socket_fd,&server_addr); // socket is connected to server 
    
     msg.type = 1;
 	msg.length = 0;
-    send(socket_fd, &msg, sizeof(msg), 0);
+    send(socket_fd, &msg, sizeof(msg), 0); // message is sent to server for requesting tcp connection
     printf("Connected to %s via TCP at port %d || Sent type '%d' message \n",inet_ntoa(server_addr.sin_addr), ntohs(server_addr.sin_port), msg.type);
-    messagerecieve(&socket_fd,&msg);
+    messagerecieve(&socket_fd,&msg);//message received from the server 
+
     printf("Connected to %s via TCP at port %d || Received type '%d' message \n",inet_ntoa(server_addr.sin_addr), ntohs(server_addr.sin_port), msg.type);
     
     udp_port = msg.length;
     printf("%s returned UDP port '%d' for connection \n",inet_ntoa(server_addr.sin_addr),  msg.length);
     printf("%s is closing TCP connection %d(0 if success)\n",inet_ntoa(server_addr.sin_addr), close(socket_fd));
-     socketcreate(&socket_fd,AF_INET, SOCK_DGRAM,0);
+     socketcreate(&socket_fd,AF_INET, SOCK_DGRAM,0); // udp socket is created 
 
 
     servlen = sizeof(server_addr);
@@ -70,14 +71,14 @@ int main(int argc, char **argv)
     
     printf("Connected to %s via UDP at port %d \nsend a message :",inet_ntoa(server_addr.sin_addr), ntohs(server_addr.sin_port));
 
-    fgets(msg.msg,sizeof(msg.msg),stdin);
+    fgets(msg.msg,sizeof(msg.msg),stdin); // takes input from user to send message
     msg.length = strlen(msg.msg);
 	msg.msg[msg.length-1]='\0';
-    sendto(socket_fd, &msg, sizeof(msg), 0,(struct sockaddr *)&server_addr, sizeof(server_addr));
+    sendto(socket_fd, &msg, sizeof(msg), 0,(struct sockaddr *)&server_addr, sizeof(server_addr)); // user message is sent to server
     
     printf("Message sent to %s via UDP at port %d of type '%d' \n",inet_ntoa(server_addr.sin_addr), ntohs(server_addr.sin_port), msg.type);
 
-    recvfrom(socket_fd,&msg,sizeof(msg),0,(struct sockaddr *)&server_addr, &servlen);
+    recvfrom(socket_fd,&msg,sizeof(msg),0,(struct sockaddr *)&server_addr, &servlen); //message is received from server
     msg.msg[msg.length]='\0';
     
     printf("%s sent a  message via UDP at port %d of type '%d' \n",inet_ntoa(server_addr.sin_addr), ntohs(server_addr.sin_port), msg.type);
--- server2.c
+++ server2.c
@@ -1,5 +1,8 @@
 #include "requirement.h"
+//all the header files is included in this.
 
+
+// function to create a new socket
 void socketcreate(int *listenfd,int domain,int type,int proto)
 {
     if ((*listenfd = socket (domain, type, proto)) <0)
@@ -8,6 +11,8 @@ void socketcreate(int *listenfd,int domain,int type,int proto)
         exit(2);
     }
 }
+
+//function to make socket ready to listen
 
 void socketlisten(int *listenfd)
 {
@@ -18,6 +23,7 @@ void socketlisten(int *listenfd)
     }
 }
 
+//function to bind socket to a port
 void socketbind(int *listenfd,struct sockaddr_in *server_addr)
 {
 
@@ -33,72 +39,81 @@ void socketbind(int *listenfd,struct sockaddr_in *server_addr)
 
 int main (int argc, char **argv)
 {
+    //if arguments are not correct return error.
 	if(argc!=2)
     {
         printf("No server port given \nensure: <filename> <server port>\n");
         exit(1);
     }
     int listenfd, connfd,serv_port = atoi(argv[1]);
-    struct message msg;
+    struct message mssg;
     pid_t child_id;
     socklen_t client_add_len;
     char buf[m_len];
     struct sockaddr_in client_addr, server_addr;
     
-    socketcreate(&listenfd,AF_INET, SOCK_STREAM,0);
+    socketcreate(&listenfd,AF_INET, SOCK_STREAM,0);//tcp socket is made
     
     server_addr.sin_family = AF_INET;
     server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
     server_addr.sin_port = htons(serv_port);
     
-    socketbind(&listenfd,&server_addr);
-    socketlisten(&listenfd);
-    int c_no = 0;
+    socketbind(&listenfd,&server_addr);// socket is binded with given port
+    socketlisten(&listenfd);// socket is ready to accept connection
+    int client_no = 0;
     
-    printf("Server is running and ready to connect ..........\n");
+    printf("Server is running and ready to connect ..........\n\n");
     for ( ; ; )
     {
         client_add_len = sizeof(client_addr);
-        connfd = accept (listenfd, (struct sockaddr *) &client_addr, &client_add_len);
+        connfd = accept (listenfd, (struct sockaddr *) &client_addr, &client_add_len); // new socket is made to accept the connection from the client
 	
-	c_no++;
+	    client_no++;
         if ( (child_id = fork ()) == 0 )  //Child process
         {
-            close (listenfd);
-            if( recv(connfd, &msg, m_len,0) == 0)
+            close (listenfd); // socket made earlier is closed 
+            if(recv(connfd, &mssg, m_len,0) == 0) //msg is received from client
             {
                 perror("The client terminated prematurely\n");
                 exit(5);
             }
-            printf("client no. %d (%s) connected via TCP at port %d\n",c_no, inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port));
-            printf("\tReceived type '%d' message from client no. %d (%s)\n", msg.type, c_no, inet_ntoa(client_addr.sin_addr));	    
-    	    server_addr.sin_port = htons(0);
-            socketcreate(&listenfd,AF_INET, SOCK_DGRAM,0);
-            socketbind(&listenfd,&server_addr);
+            printf("client no. %d (%s) connected via TCP at port %d\n",client_no, inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port));
+            printf("\tReceived type '%d' message from client no. %d (%s)\n", mssg.type, client_no, inet_ntoa(client_addr.sin_addr));	    
+    	    server_addr.sin_port = htons(0); // arbitrary a port is assigned for udp
+
+            socketcreate(&listenfd,AF_INET, SOCK_DGRAM,0); // udp socket is created 
+            socketbind(&listenfd,&server_addr); // udp port is binded to the socket
     	    struct sockaddr_in localAddress;
     	    socklen_t addressLength = sizeof localAddress;
-    	    getsockname(listenfd, (struct sockaddr*)&localAddress,&addressLength);    	    
-            printf("\tPort %d assigned for UDP connection with client no. %d (%s) \n",(int) ntohs(localAddress.sin_port),c_no, inet_ntoa(client_addr.sin_addr));
-    	    msg.type = 2;    		
-            msg.length = ntohs(localAddress.sin_port);
-            send(connfd, &msg, sizeof(msg), 0);    	    
-            printf("\tsent type '%d' message to client no. %d (%s)\n", msg.type,c_no, inet_ntoa(client_addr.sin_addr));
-            printf("client no. %d (%s) TCP connection at port %d CLOSED %d(0 for success)\n",c_no, inet_ntoa(client_addr.sin_addr),ntohs(client_addr.sin_port), close(connfd));
-    	    recvfrom(listenfd,&msg,sizeof(msg),0,(struct sockaddr *)&client_addr, &client_add_len);
-    	    msg.msg[msg.length]='\0';
-            printf("client no. %d (%s) connected via UDP at port %d || Received type '%d' message: '%s'\n",c_no, inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port), msg.type, msg.msg);           
-    	    // printf("client no %d (%s) || received message at UDP port(%d) : %s\n",c_no, inet_ntoa(client_addr.sin_addr),ntohs(client_addr.sin_port), );
-    	    msg.type=4;
-    	    sendto(listenfd, &msg, sizeof(msg), 0,(struct sockaddr *)&client_addr, client_add_len);
-            printf("\tsent type '%d' message via UDP to client no. %d (%s)\n",msg.type, c_no, inet_ntoa(client_addr.sin_addr));
-            printf("Terminating Connection with client no. %d (%s) \n",c_no,inet_ntoa(client_addr.sin_addr));
+    	    getsockname(listenfd, (struct sockaddr*)&localAddress,&addressLength);  // the port which was assigned earlier is retrieved and stored in localAddress
+
+            printf("\tPort %d assigned for UDP connection with client no. %d (%s) \n",(int) ntohs(localAddress.sin_port),client_no, inet_ntoa(client_addr.sin_addr)); 
+    	    mssg.type = 2;    		
+            mssg.length = ntohs(localAddress.sin_port); // udp port is saved in length variable of struct message 
+            send(connfd, &mssg, sizeof(mssg), 0);  // message is sent to client with udp port number  	    
+            printf("\tsent type '%d' message to client no. %d (%s)\n\n", mssg.type,client_no, inet_ntoa(client_addr.sin_addr));
+            printf("client no. %d (%s) TCP connection at port %d CLOSED %d(0 for success)\n\n",client_no, inet_ntoa(client_addr.sin_addr),ntohs(client_addr.sin_port), close(connfd));//tcp connection is closed
+    	    
+            if(recvfrom(listenfd,&mssg,sizeof(mssg),0,(struct sockaddr *)&client_addr, &client_add_len) == 0) // message is received from the client 
+            {
+                perror("The client terminated prematurely\n");
+                exit(5);
+            }
+
+    	    mssg.msg[mssg.length]='\0';
+            printf("client no. %d (%s) connected via UDP at port %d || Received type '%d' message: '%s'\n",client_no, inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port), mssg.type, mssg.msg);           
+    	    mssg.type=4;
+    	    sendto(listenfd, &mssg, sizeof(mssg), 0,(struct sockaddr *)&client_addr, client_add_len); // server sending message to client via udp 
+            printf("\tsent type '%d' message via UDP to client no. %d (%s)\n",mssg.type, client_no, inet_ntoa(client_addr.sin_addr));
+            printf("Terminating Connection with client no. %d (%s) \n\n",client_no,inet_ntoa(client_addr.sin_addr)); // udp connection is closed
     	    fflush(stdout);
     	    
     	    return 0;
         }
-        close(connfd);
+        close(connfd); //if a child is not created properly then close the socket to accept connection
         
     }
-    close(listenfd);
+    close(listenfd); //  udp connection is closed when work is completed
     return 0;
 }
+
